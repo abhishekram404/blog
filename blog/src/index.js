bb7e38940f957558d6897a0bdf8d9db443578fe9
Keshav Kishor Ram <55845306+abhishekram404@users.noreply.github.com>
2022-01-22 17:09:34 +0545

    Set isProduction=true for testing purpose

--- blog/src/index.js
+++ blog/src/index.js
@@ -7,7 +7,8 @@ import store from "redux/store";
 import axios from "axios";
 import { transitions, positions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
-const isProduction = process.env.REACT_APP_NODE_ENV === "production";
+// const isProduction = process.env.REACT_APP_NODE_ENV === "production";
+const isProduction = true;
 axios.defaults.baseURL = isProduction
   ? "https://react-blog-restapi.herokuapp.com/api/"
   : "http://localhost:4000/api/";
