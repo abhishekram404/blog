ad2ef8be404416016c9a54ca7c249271e047e3fc
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-02-02 15:20:49 +0545

    User react-query in Post component and fix bugs

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,28 +1,43 @@
 import clsx from "clsx";
-import React, { Suspense } from "react";
+import React from "react";
 import Loading from "./Loading";
 import "styles/post.scss";
 import { useDispatch, useSelector } from "react-redux";
 import parser from "html-react-parser";
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
-import { fetch_a_post } from "redux/actions/postActions";
 import moment from "moment";
+import { useQuery } from "react-query";
+import axios from "axios";
+import { ERROR } from "redux/constants";
 export default function Post({ title, body, category, preview = false }) {
   const { dark } = useSelector((state) => state.common);
   const { id } = useParams();
   const dispatch = useDispatch();
-  const visitedPosts = useSelector((state) =>
-    state.post.visitedPosts.filter((post) => post._id === id)
-  );
 
-  const [post] = visitedPosts;
-  useEffect(() => {
-    if (visitedPosts.some((post) => post._id === id)) {
-      return;
-    }
-    dispatch(fetch_a_post(id));
-  }, []);
+  let {
+    isLoading,
+    isError,
+    data: post,
+  } = useQuery("fetchedPost", () => axios.get(`/post/fetch?id=${id}`), {
+    enabled: !preview,
+  });
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    dispatch({
+      type: ERROR,
+      payload: "Something went wrong while fetching the post.",
+    });
+  }
+  if (!preview) {
+    post = post.data.details;
+  } else {
+    post = { title, category, content: body };
+  }
+
   return (
     <div
       className={clsx(
@@ -36,17 +51,17 @@ export default function Post({ title, body, category, preview = false }) {
           <div className={clsx("title-bar row align-items-start ")}>
             <div className="col-12 title-row">
               <h1 className="post-title">
-                {post ? post.title : "Post title goes here..."}
+                {post.title || "Post title goes here..."}
               </h1>
             </div>
           </div>
           <div className="info-row mb-3">
             <span className="author-col">
-              Article by <b>{post ? post.author.authorName : "Author name"}</b>
+              Article by <b>{post?.author?.authorName || "Author name"}</b>
             </span>
             &nbsp; &bull; &nbsp; Published on{" "}
-            {post ? moment(post.createdAt).format("LLL") : "Time"} &nbsp; &bull;
-            &nbsp; {post ? post.category : "Undefined"}
+            {post.createdAt ? moment(post.createdAt).format("LLL") : "Time"}{" "}
+            &nbsp; &bull; &nbsp; {post.category || "Undefined"}
           </div>
 
           <div className="post-body">
