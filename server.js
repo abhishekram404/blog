010cf0aec9cb8de89e3fc96ece2723f3c0cb5fc2
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-02-08 17:51:58 +0545

    Rebuild react app after adding .env.production and .env.development files

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use("/api/post", postRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
   app.get("*", async (req, res) => {
-    return res.sendFile(__dirname, "client", "build", "index.html");
+    res.sendFile(__dirname, "client", "build", "index.html");
   });
 }
 
